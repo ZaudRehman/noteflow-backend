4461fb473dd75a1bf9a7efb2a4bfb2a67b966083
ZaudRehman <zaudrehman@gmail.com>
2025-08-06 14:18:54 +0530

    Adding CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, notes, websocket
 
 app = FastAPI(title="NoteFlow Backend")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # For development. For security in production, use your deployed frontend URL, e.g. ["https://yourfrontend.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(notes.router)
